42692fdb1ff37aef7f79a02eb6454d9c6721d7ef
Andrew Davis <addavis@gmail.com>
2025-04-28 23:37:05 -0400

    toggle open files, if necessary

--- toggle_sidebar_focus.py
+++ toggle_sidebar_focus.py
@@ -7,4 +7,10 @@ class ToggleSidebarAndFocusCommand(sublime_plugin.WindowCommand):
             self.window.run_command("toggle_side_bar")
         else:
             self.window.run_command("toggle_side_bar")
+
+            # if it's still not visible, try showing open files
+            if not self.window.is_sidebar_visible():
+                self.window.run_command("toggle_show_open_files")
+                self.window.run_command("toggle_side_bar")
+
             self.window.run_command("focus_side_bar")
