9f38f746c3aed0b888a5dd0f14dc10c227af8969
Andrew Davis <addavis@gmail.com>
2025-04-28 23:37:54 -0400

    whitespace only

--- toggle_sidebar_focus.py
+++ toggle_sidebar_focus.py
@@ -2,9 +2,12 @@ import sublime
 import sublime_plugin
 
 class ToggleSidebarAndFocusCommand(sublime_plugin.WindowCommand):
+
     def run(self):
+
         if self.window.is_sidebar_visible():
             self.window.run_command("toggle_side_bar")
+
         else:
             self.window.run_command("toggle_side_bar")
 
